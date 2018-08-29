c1e1271a3055fd7fa9b44c88de460fa06c1d9632
Nathan Tofte <nathan.tofte@gmail.com>
2018-08-28 19:25:35 -0500

    make variables more descriptive

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,37 +1,50 @@
 #include <Servo.h>
 
-// These constants won't change. They're used to give names to the pins used:
-const int m1Black = A0;  // Analog input pin that the potentiometer is attached to
-const int m1Red = A1;
-const int m2Black = A2;  // Analog input pin that the potentiometer is attached to
-const int m2Red = A3;
+/*
+Converts DC H-Bridge motor controller output to a hobby ESC control.
+1.Simulates a DC motor with a resistor divider
+2.Measures voltage on both leads on the DC motor
+3.Determines % of power intended to drive the motor
+4.Sets the positions the Servo responsible for that motor's ESC.
 
+Created by Nathan Tofte (HWSWFUN)
+August 28, 2018
+*/
 
-int m1ServoOut = 90;
+const int motor1Black = A0;   // Analog input pin for black wire of motor 1
+const int motor1Red = A1;     // Analog input pin for red wire of motor 1
+const int motor2Black = A2;   // Analog input pin for black wire of motor 2
+const int motor2Red = A3;     // Analog input pin for red wire of motor 2
 
-Servo leftServo;  // create servo object to control a servo
-Servo rightServo;
+const int motor1ESC = D8;     // Pin connected to ESC for motor 1
+const int motor2ESC = D9;     // Pin connected to ESC for motor 2
+
+Servo motor1ESC;  // Servo output for motor 1.  Connect to motor 1 Electronic Speed Control.
+Servo motor2ESC;  // Servo output for motor 1.  Connect to motor 1 Electronic Speed Control.
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-  leftServo.attach(8);
-  rightServo.attach(9);
+  motor1ESC.attach(motor1ESC);
+  motor2ESC.attach(motor2ESC);
 }
 
+int fullSpeedOffset = 45; // Add to 90 to get full speed
+int speedScale = 3;       // Hobby motor can be super fast so scale the speed to only a portion.
+
 void loop() {
-  float m1Value = readStick(m1Black, m1Red);
-  float m2Value = readStick(m2Black, m2Red);
+  float motor1Value = readStick(motor1Black, motor1Red);
+  float motor2Value = readStick(motor2Black, motor2Red);
 
-  int sLeft = 90 + 15 * m1Value;
+  int sLeft = 90 + (fullSpeedOffset / speedScale) * motor1Value;
   Serial.print(sLeft);
-  leftServo.write(sLeft);
+  motor1ESC.write(sLeft);
 
-  int sRight = 90 + -15 * m2Value;  // instead of 15, can go about 40 to allow full speed.  this way it is limited.
+  int sRight = 90 + -(fullSpeedOffset / speedScale) * motor2Value;
   Serial.println("\t"+sRight);
-  rightServo.write(sRight);
+  motor2ESC.write(sRight);
 
-  // wait 2 milliseconds before the next loop for the analog-to-digital
+  // wait 20 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
   delay(20);
 }
@@ -49,25 +62,17 @@ float readStick(int blackAnalogPin, int redAnalogPin) {
     // backward
     ret = map(blackValue, 0, 250, -1000, 0) / 1000.0f;
   } else ret = 0.0f;
-  return ret;  
+  return ret;
 }
 
-
-
 /*
- * For motor 1.  2 analog inputs because monitor red and black wire independantly.
-                A0 (black)    A1 (red)
-full up         678           0
-above neutral - 678           237
-Neutral -       0             0
-below neutral - 247           665
-full down -     0             665
+2 analog inputs to monitor red and black wires independantly.
+These are sample A/D readings
 
-  m1BlackValue = analogRead(m1Black);
-  m1RedValue = analogRead(m1Red);
-
-
-
+                      A0 (black)    A1 (red)
+full up               678           0
+just above neutral -  678           237
+Neutral -             0             0
+just below neutral -  247           665
+full down -           0             665
 */
-
-
